a6fa1dcad082b149d04d18ff38588e8c74b246b7
Walter Carvalho <waltervascarvalho@gmail.com>
2013-12-19 01:16:27 +0000

    prototype

---
+++ app.js
@@ -0,0 +1,59 @@
+/* Deps */
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    app = express(),
+    poet = require('poet')(app);
+
+/* poet */
+poet
+  .watch(function () {
+    poet.clearCache();
+  })
+  .init();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.locals.moment = require('moment');
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', function (req, res, nxt) {
+  var posts = poet.helpers.getPosts(0, 5),
+      latest = posts.shift(),
+      summary = getSummary(latest);
+
+  res.render('index', {posts: posts, title: 'Home', latest: { post: latest, summary: summary } })
+});
+
+poet.addRoute('/written/:post', function (req, res, nxt) {
+  var post = poet.helpers.getPost(req.params.post);
+  if (post) {
+    res.render('post', { post: post, title: post.title });
+  } else {
+    res.send(404);
+  }
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+var getSummary = function (post) {
+  var summary = new String(post.content);
+  summary = summary.substr(0, 250) + '... <a href=\'' + post.url + '\'> more <i class=\'ion-arrow-right-c\'></i>';
+  return summary;
+}
+
---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+(function () {
+
+  console.debug('Hi! You can find the source code for this website @ http://www.github.com/waltervascarvalho');
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-30089530-1', 'waltercarvalho.com');
+    ga('send', 'pageview');
+
+})();
